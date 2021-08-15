b7b76a8ebef91f5ba2a72121613b8c798cf8eb3e
farque65 <fahim.farque@gmail.com>
2021-08-15 13:20:58 -0400

    stabilized refactor

--- pages/_app.js
+++ pages/_app.js
@@ -1,32 +1,7 @@
-import App from 'next/app'
 import '../styles/globals.css'
-import React from 'react'
-import { SourceProvider } from '../components/source'
 
-let store = {};
-
-const globalStore = {};
-
-globalStore.set = (key, value) => {
-    store = { ...store, [key]: value };
-}
-
-globalStore.get = (key) => {
-    return store[key];
-}
-
-
-export default globalStore;
-
-class MyApp extends App {
-  render() {
-    const { Component, pageProps } = this.props
-    return (
-      <SourceProvider>
-        <Component {...pageProps} />
-      </SourceProvider>
-    )
-  }
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
 }
 
 export default MyApp
